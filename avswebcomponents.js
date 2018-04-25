004a03afb3170a1705afc617168da78707ef5fe6
AVS-DEV <support@avs.com>
2018-04-25 15:15:24 -0400

    Updated license strings and headers.
    Copied from Perforce
     Change: 65653

--- avswebcomponents.js
+++ avswebcomponents.js
@@ -1,19 +1,22 @@
-// -------------------------------------------------------------------------//
-//-*- javascript -*-
-//
-//                   Copyright (c) 1998-2018 by
-//                   Advanced Visual Systems Inc.
-//                   All Rights Reserved
-//
-//   This software comprises unpublished confidential information of
-//   Advanced Visual Systems Inc. and may not be used, copied or made
-//   available to anyone, except in accordance with the license
-//   under which it is furnished.
-//
-//   This file is under Perforce control at AVS in:
-//   $Id: //depot/dvc/internal/Research/AVSWebComponents/DemoApp/client/js/avs_web_components.js#1 $
-//
-// --------------------------------------------------------------------------//
+/*
+avswebcomponents.js
+Copyright 2018 Advanced Visual Systems Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+This product includes software developed at
+Advanced Visual Systems Inc. (http://www.avs.com)
+*/
 
 function addPickListener(callback) {
         var viewerImage = document.querySelector('avs-viewer-image');
