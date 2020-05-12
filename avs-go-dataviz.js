7a50a3ee3d3364073bca401961ce1d1b89d039be
Owen Dimond <odimond@avs.com>
2020-05-12 11:12:45 -0400

    Updated logo. Adjust spinner animation. Handle non-streamed json file correctly. 0.2.67340

--- avs-go-dataviz.js
+++ avs-go-dataviz.js
@@ -70,9 +70,9 @@ class AvsGoDataViz extends AvsDataSourceMixin(AvsStreamMixin(AvsHttpMixin(mixinB
           transform:translate(-50%,-50%);
         }
         .spin {
-          -webkit-animation:spin 4s linear infinite;
-           -moz-animation:spin 4s linear infinite;
-           animation:spin 4s linear infinite;
+          -webkit-animation:spin 1s ease-in-out infinite;
+           -moz-animation:spin 1s ease-in-out infinite;
+           animation:spin 1s ease-in-out infinite;
         }
         @-moz-keyframes spin { 100% { -moz-transform: rotate(360deg); } }
         @-webkit-keyframes spin { 100% { -webkit-transform: rotate(360deg); } }
@@ -738,6 +738,9 @@ class AvsGoDataViz extends AvsDataSourceMixin(AvsStreamMixin(AvsHttpMixin(mixinB
           }
         }
       }
+    }
+    else if (this.urlLoadJsonFile) {
+      this.threeViewer.loadGeometryAsJson(json);
     }
 
     if (this.spinner !== undefined) {
