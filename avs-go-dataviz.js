f7c0417b42f78a2a9416c9efe87bcce0ba9e6bac
Owen Dimond <odimond@avs.com>
2020-05-13 15:37:13 -0400

    Slow spinner to 2s and size down to 40x40px 0.2.67355

--- avs-go-dataviz.js
+++ avs-go-dataviz.js
@@ -79,9 +79,9 @@ class AvsGoDataViz extends AvsDataSourceMixin(AvsStreamMixin(AvsHttpMixin(mixinB
           transform:translate(-50%,-50%);
         }
         .spin {
-          -webkit-animation:spin 1.25s ease-in-out infinite;
-           -moz-animation:spin 1.25s ease-in-out infinite;
-           animation:spin 1.25s ease-in-out infinite;
+          -webkit-animation:spin 2s ease-in-out infinite;
+           -moz-animation:spin 2s ease-in-out infinite;
+           animation:spin 2s ease-in-out infinite;
         }
         @-moz-keyframes spin { 100% { -moz-transform: rotate(360deg); } }
         @-webkit-keyframes spin { 100% { -webkit-transform: rotate(360deg); } }
